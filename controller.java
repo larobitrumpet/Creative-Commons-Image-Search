513b9409f7cb99d3af2de1e7387d2d9e1009dce1
Luke Robillard <lrobillard@sierracollege.edu>
2021-05-08 23:14:29 -0700

    Add controller.java

---
+++ controller.java
@@ -0,0 +1,171 @@
+/*
+ * WxController
+ * This is the controller for the FXML document that contains the view. 
+ */
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+/**
+ * @author Luke Robillard
+ */
+
+public class WxController implements Initializable
+{
+    @FXML
+    private CheckBox ckbCommercially;
+    @FXML
+    private CheckBox ckbModify;
+    @FXML
+    private CheckBox ckbCC0;
+    @FXML
+    private CheckBox ckbPublicDomain;
+    @FXML
+    private CheckBox ckbBY;
+    @FXML
+    private CheckBox ckbBY-SA;
+    @FXML
+    private CheckBox ckbBY-NC;
+    @FXML
+    private CheckBox ckbBY-ND;
+    @FXML
+    private CheckBox ckbBY-NC-SA;
+    @FXML
+    private CheckBox ckbBY-NC-ND;
+    @FXML
+    private CheckBox ckbADW;
+    @FXML
+    private CheckBox ckbBHL;
+    @FXML
+    private CheckBox ckbBM;
+    @FXML
+    private CheckBox ckbCAPL;
+    @FXML
+    private CheckBox ckbCMA;
+    @FXML
+    private CheckBox ckbDA;
+    @FXML
+    private CheckBox ckbDM;
+    @FXML
+    private CheckBox ckbE;
+    @FXML
+    private CheckBox ckbF;
+    @FXML
+    private CheckBox ckbFO;
+    @FXML
+    private CheckBox ckbGBI;
+    @FXML
+    private CheckBox ckbMCM;
+    @FXML
+    private CheckBox ckbMMA;
+    @FXML
+    private CheckBox ckbMV;
+    @FXML
+    private CheckBox ckbNASA;
+    @FXML
+    private CheckBox ckbNYPL;
+    @FXML
+    private CheckBox ckbPP;
+    @FXML
+    private CheckBox ckbR;
+    @FXML
+    private CheckBox ckbRM;
+    @FXML
+    private CheckBox ckbSM;
+    @FXML
+    private CheckBox ckbS;
+    @FXML
+    private CheckBox ckbSINMAAHC;
+    @FXML
+    private CheckBox ckbSINMAA;
+    @FXML
+    private CheckBox ckbSINASM;
+    @FXML
+    private CheckBox ckbSISAAM;
+    @FXML
+    private CheckBox ckbSINMAH;
+    @FXML
+    private CheckBox ckbSINMAI;
+    @FXML
+    private CheckBox ckbSIACM;
+    @FXML
+    private CheckBox ckbSIHMSG;
+    @FXML
+    private CheckBox ckbSIA;
+    @FXML
+    private CheckBox ckbSISL;
+    @FXML
+    private CheckBox ckbSINMNH;
+    @FXML
+    private CheckBox ckbSINPG;
+    @FXML
+    private CheckBox ckbSINPM;
+    @FXML
+    private CheckBox ckbSINZ;
+    @FXML
+    private CheckBox ckbSX;
+    @FXML
+    private CheckBox ckbSVGS;
+    @FXML
+    private CheckBox ckbT;
+    @FXML
+    private CheckBox ckbTM;
+    @FXML
+    private CheckBox ckbWC;
+    @FXML
+    private CheckBox ckbWOCTC;
+    @FXML
+    private CheckBox ckbWRMS;
+    @FXML
+    private CheckBox ckbPhotos;
+    @FXML
+    private CheckBox ckbIllustrations;
+    @FXML
+    private CheckBox ckbDigitizedArt;
+    @FXML
+    private CheckBox ckbJPEG;
+    @FXML
+    private CheckBox ckbPNG;
+    @FXML
+    private CheckBox ckbGIF;
+    @FXML
+    private CheckBox ckbSVG;
+    @FXML
+    private CheckBox ckbTall;
+    @FXML
+    private CheckBox ckbWide;
+    @FXML
+    private CheckBox ckbSquare;
+    @FXML
+    private CheckBox ckbSmall;
+    @FXML
+    private CheckBox ckbMedium;
+    @FXML
+    private CheckBox ckbLarge;
+    @FXML
+    private CheckBox ckbCreator;
+    @FXML
+    private TextField txtSearch;
+    @FXML
+    private Button btnSearch;
+
+    @FXML
+    private void btnSearchPressed(ActionEvent e)
+    {
+        //TODO
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb)
+    {
+        //TODO
+    }
+}
