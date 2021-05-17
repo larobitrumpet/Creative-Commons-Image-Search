f533c16d49539b7e9ae1d6069581df48dac0eaac
Luke Robillard <lrobillard@sierracollege.edu>
2021-05-17 12:21:45 -0700

    Create CCISMoreController.java to control CCISViewMore.fxml and connect CCISViewMore.fxml to CCISMoreController

---
+++ CCISMoreController.java
@@ -0,0 +1,30 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class CCISMoreController implements Initializable
+{
+    private CCISModel model = new CCISModel();
+
+    @FXML
+    private Label lblTitle;
+    @FXML
+    private Label lblName;
+    @FXML
+    private Hyperlink hylCratorURL;
+    @FXML
+    private Label lblLicence;
+    @FXML
+    private Label lblAttribution;
+    @FXML
+    private ImageView imgImage;
+    @FXML
+    private Button btnDownload;
+}
